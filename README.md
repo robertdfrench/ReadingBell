98609c0f5b98500cd117f7d49aa763eb40312e1d
Robert D. French <roberto.frances@gmail.com>
2011-09-15 10:59:28 -0500

    Reading Bell utility

---
+++ README.md
@@ -0,0 +1,4 @@
+Reading Bell
+============
+
+Perl utility for slogging through large, uninteresting documents in a timely fashion. Dings a bell every N seconds and reminds you what page to be on, in case you get distracted. Great for skimming textbooks or boring documentation for languages like Moonscript.
---
+++ readingBell.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+if (scalar(@ARGV) < 3) {
+	print "Usage: \n";
+	print "\t ./readingTimer.pl [start_page] [num_pages_to_read] [num_seconds_per_page]\n";
+}
+$start_page = $ARGV[0];
+$num_pages = $ARGV[1];
+$seconds_per_page = $ARGV[2];
+
+print "Reading from $start_page to " . ($start_page + $num_pages - 1) . " in " . ($seconds_per_page * $num_pages) . " seconds.\n";
+
+for(my $offset = 0; $offset < $num_pages; $offset++) {
+	print "\aYou should be on page " . ($start_page + $offset) . "\n";
+	sleep($seconds_per_page);
+}
+print "\a\a\a STOP READING \a\a\a\n";
